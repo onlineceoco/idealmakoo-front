676f657e2060c74153842a40bf01c30e08b7ba76
Online Ceo <89314537+onlineceoco@users.noreply.github.com>
2021-10-06 15:36:31 +0330

    mosi

--- pages/_app.js
+++ pages/_app.js
@@ -8,17 +8,17 @@ import BetweenPage from '../components/Loading/betweenPage'
 function MyApp({ Component, pageProps }) {
   const [loading,setLoading]=useState(false)
   Router.events.on('routeChangeStart',(url)=>{
-    console.log('route is changing')
+    //console.log('route is changing')
     setLoading(true)
   })
   Router.events.on('routeChangeComplete',(url)=>{
-    console.log('route is complete')
+    //console.log('route is complete')
     setLoading(false)
   })
   return (
     <>
     {loading && <BetweenPage/>}
-    <Component {...pageProps} />
+     {!loading &&<Component {...pageProps} />}
     </>
   )
 }
